b6f3fccfdeee11743d5fdfc6cd2ebb2d8235b008
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-04 09:23:08 +0600

    loop

--- hello.c
+++ hello.c
@@ -31,3 +31,53 @@
 //     printf("%d*%d =%d\n",num1,num2,num1*num2);
 //     printf("%d/%d=%d\n", num1, num2, num1 / num2);
 // }
+
+// Enter a value;
+
+// conditional logic
+
+
+// int main(){
+//     int n;
+//     n = 10;
+
+//         if(n>0){
+//             printf("the number is positive");
+//         }else if(n==0){
+//             printf("the number is equal");
+
+//         }else{
+//             printf("the number is negetive");
+//         }
+// }
+
+// remainder;
+
+
+
+// int main(){
+
+//     int numbr, raminder;
+
+//     numbr = 5;
+//     raminder = numbr % 2;
+
+//     if(raminder==0){
+//         printf("the number is even");
+//     }else{
+//         printf("the number is odd");
+//     };
+// }
+
+// Or Oparator;
+
+
+
+// int main(){
+//     int num = 10;
+//     if(num>=1 || num<=10){
+//         printf("yes/n");
+//     }else{
+//         printf("yes/n");
+//     }
+// }

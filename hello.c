506e109c174b5a8a49605fd429f42f69db505592
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-04 08:09:28 +0600

    hello World

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// int main(){
+//     char ch;
+
+//     printf("Enter frist Latter of your Name :");
+//     scanf("%c", &ch);
+//     printf("The first letter of your name is %c\n ", ch);
+// // }
+
+// int main(){
+
+//     char ch;
+//     printf("Enter frist Letter of your Name");
+//     ch = getchar();
+//     printf("the first letter of your name :%c\n", ch);
+
+//     return 0;
+// }
